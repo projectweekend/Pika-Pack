fb7d2d64073f59acf5b54ee2c44b44283374390e
Brian Hines <brian@projectweekend.net>
2015-02-15 17:39:45 -0600

    Pip stuff

---
+++ README.txt
@@ -0,0 +1,7 @@
+==================
+Pika-Pack
+==================
+
+Handy components when working with RabbitMQ and Pika (https://pika.readthedocs.org/en/latest/)
+
+For documentation: https://github.com/projectweekend/Pika-Pack
---
+++ setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+
+setup(
+    name='Pika-Pack',
+    version='0.0.1',
+    author='Brian Hines',
+    author_email='brian@projectweekend.net',
+    packages=['pika_pack'],
+    url='https://github.com/projectweekend/Pika-Pack',
+    license='LICENSE.txt',
+    description='Handy components when working with RabbitMQ and Pika (https://pika.readthedocs.org/en/latest/).',
+    long_description=open('README.txt').read(),
+    install_requires=[
+        "pika == 0.9.14",
+        "pyecho == 0.0.2",
+    ],
+)
