37d1c5dfd961a260e75197479bad64c431714c32
Brian Hines <brian@projectweekend.net>
2015-05-02 18:27:10 -0500

    Publish update

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='Pika-Pack',
-    version='1.0.4',
+    version='2.0.0',
     author='Brian Hines',
     author_email='brian@projectweekend.net',
     packages=['pika_pack'],
